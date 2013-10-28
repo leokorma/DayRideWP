6c4df0706efea3ac098ac55ce91ae487c9d3dbba
leokorma <leokorma@gmail.com>
2013-10-28 08:45:10 +0200

    Added NetworkMeter project

---
+++ WindowsPhone/NetworkMeter/NetworkMeter/Localization/LocalizedStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace NetworkMeter.Localization
+{
+    // Class to enable localization
+    public class LocalizationUtils
+    {
+        // Constructor
+        public LocalizationUtils()
+        {
+        }
+
+        private static LocalizationResources _localizationResources = new LocalizationResources();
+
+        public LocalizationResources LocalizationResources
+        {
+            get { return _localizationResources; }
+        }
+    }
+}
---
+++ WindowsPhone/NetworkMeter/NetworkMeter/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.ComponentModel;
+using System.Windows.Controls.Primitives;
+using System.Threading;
+
+namespace NetworkMeter
+{
+    public partial class LoginPage : PhoneApplicationPage
+    {
+        // Constructor
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
